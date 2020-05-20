9de4c01820a13a50128a6baa6e30200894b27641
irvingvillasenor <irving@pipersolution.com>
2020-05-20 15:28:07 -0400

    add attributes and print

--- store_item.rb
+++ store_item.rb
@@ -1,3 +1,40 @@
-hot_sauce = {name: "Mad Dog 357", scoville_units: 9000000000, price: 11.75}
-hot_sauce = {name: "The Source Hot Sauce", scoville_units: 7100000000, price: 18}
-hot_sauce = {:name => "Get Bitten", :scoville_units => 6000000000, :price => 16.95}
+# hot_sauce = {name: "Mad Dog 357", scoville_units: 9000000000, price: 11.75}
+# hot_sauce = {name: "The Source Hot Sauce", scoville_units: 7100000000, price: 18}
+# hot_sauce = {:name => "Get Bitten", :scoville_units => 6000000000, :price => 16.95}
+
+class Hot_sauce
+
+  attr_reader :name, :scoville_units, :price
+  attr_writer :price
+
+  def initialize(name, scoville_units, price)
+    @name= name
+    @scoville_units= scoville_units
+    @price= price
+
+    # def name
+    #   @name
+    # end
+
+    # def scoville_units
+    #   @scoville_units
+    # end
+
+    # def price
+    #   @price
+    # end
+
+    # def price(new_price)
+    #   @price = new_price
+    # end
+
+  end
+end
+
+hot_sauce1 = Hot_sauce.new("Mad Dog 357", 9000000000, 11.95)
+p hot_sauce1
+hot_sauce1.price= 40
+p hot_sauce1
+p hot_sauce1.name
+p hot_sauce1.scoville_units
+p hot_sauce1.price
